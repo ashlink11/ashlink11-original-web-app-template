b5e6710595539e285de645c3877248426d303e28
ashtreehugs <agrev8@gmail.com>
2020-05-01 13:35:36 -0400

    Major success - React app deploys live to gh pages

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> Welcome to a React app built with yarn and parcel and deployed with GitHub pages</h1>
       </div>
     );
   }
